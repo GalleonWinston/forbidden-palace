ae7487077bf8008d4ea06642d07e3c09843d1543
GalleonWinston <galleonWinston1234@gmail.com>
2025-08-10 21:01:09 +0800

    create a build command

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,41 @@
-const express  = require('express')
-const dotenv = require('dotenv')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const path = require("path");
 
 dotenv.config();
 
+const authRoutes = require("./src/routes/auth.route");
+const esp32Routes = require("./src/routes/esp32.route");
 
-const authRoutes = require("./src/routes/auth.route")
-const esp32Routes = require("./src/routes/esp32.route")
-
- 
-const app = express()
+const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true, // Allow cookies to be sent with requests
-}))
+  })
+);
 
-app.get("/", (req,res) => {
-    res.status(201).json({ message: "hello World"})
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "hello World" });
+});
 
-app.use("/api/auth", authRoutes)
-app.use("/api/esp32", esp32Routes)
-
+app.use("/api/auth", authRoutes);
+app.use("/api/esp32", esp32Routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
