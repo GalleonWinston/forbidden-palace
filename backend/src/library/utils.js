f1bc211b647a46a920fc8ef8ffc0246d0ed2ebe4
GalleonWinston <galleonWinston1234@gmail.com>
2025-08-17 22:30:14 +0800

    changes some utils

--- backend/src/library/utils.js
+++ backend/src/library/utils.js
@@ -8,8 +8,8 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // JS cannot access the cookie
-    sameSite: "strict", // cookie will only be sent in a first-party context
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none", // CHANGED: Allow cross-site cookies
+    secure: true, // CHANGED: Always true for cross-site cookies
   });
 
   return token;
